f1a1f23bd5994f708c60be5a49894f6fb42787ef
Laurie Cai <laurie801@gmail.com>
2023-11-27 16:37:39 -0800

    Initial Commit

---
+++ Seedling/SeedlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeedlingApp.swift
+//  Seedling
+//
+//  Created by Laurie Cai on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SeedlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
