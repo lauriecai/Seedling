e565b9454a959504a0cb5466b18ea2238f5a5c08
Laurie Cai <laurie801@gmail.com>
2023-11-28 19:57:54 -0800

    Add weather row outline

--- Seedling/Core/Home/Views/HomeView.swift
+++ Seedling/Core/Home/Views/HomeView.swift
@@ -10,11 +10,52 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
 		ZStack {
+			Color.theme.backgroundPrimary
+				.ignoresSafeArea()
 			
+			VStack {
+				weatherRow
+			}
 		}
     }
 }
 
 #Preview {
     HomeView()
+}
+
+extension HomeView {
+	
+	private var weatherRow: some View {
+		HStack(spacing: 10) {
+			weatherCard
+			weatherCard
+			weatherCard
+			weatherCard
+			weatherCard
+		}
+		.frame(maxWidth: .infinity)
+	}
+	
+	private var weatherCard: some View {
+		VStack(spacing: 5) {
+			Text("Fri")
+				.font(.handjet(.regular, size: 18))
+			
+			Rectangle()
+				.fill(Color.yellow)
+				.frame(width: 40, height: 30)
+			
+			HStack {
+				Text("77")
+					.font(.handjet(.bold, size: 18))
+				Text("56")
+					.font(.handjet(.regular, size: 18))
+			}
+		}
+		.padding(.vertical, 5)
+		.padding(.horizontal, 10)
+		.background(Color.white)
+		.cornerRadius(8)
+	}
 }
