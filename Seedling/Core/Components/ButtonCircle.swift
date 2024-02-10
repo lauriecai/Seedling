474274f0c269cfa02440c296a9f9eb5b9d4ac347
Laurie Cai <laurie801@gmail.com>
2024-02-10 09:50:04 -0800

    Create circle button component

--- Seedling/Core/Components/ButtonCircle.swift
+++ Seedling/Core/Components/ButtonCircle.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct ButtonCircle: View {
+	
+	let icon: String
+	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		Button {
+			print("button pressed")
+		} label: {
+			Image(icon)
+				.foregroundStyle(Color.black)
+				.frame(width: 65, height: 65)
+				.background(Color.theme.accentGreen)
+				.clipShape(Circle())
+				
+		}
     }
 }
 
-#Preview {
-    ButtonCircle()
+#Preview(traits: .sizeThatFitsLayout) {
+	ZStack(alignment: Alignment(horizontal: .trailing, vertical: .bottom)) {
+		Color.white
+		
+		ButtonCircle(icon: "PlusIcon")
+			.padding(.horizontal, 20)
+			.padding(.bottom, 20)
+	}
 }
