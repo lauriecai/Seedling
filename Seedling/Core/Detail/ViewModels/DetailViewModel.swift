c883ff07e1a02b5f2f1ad92e9c798f2300deb565
Laurie Cai <laurie801@gmail.com>
2024-02-11 09:18:02 -0800

    Create file structure for DetailView and DetailViewModel

---
+++ Seedling/Core/Detail/ViewModels/DetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewModel.swift
+//  Seedling
+//
+//  Created by Laurie Cai on 2/11/24.
+//
+
+import Foundation
---
+++ Seedling/Core/Detail/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Seedling
+//
+//  Created by Laurie Cai on 2/11/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
