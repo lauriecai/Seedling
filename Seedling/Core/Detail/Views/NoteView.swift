172792440022d7d43ab4e0757ca71d875313881e
Laurie Cai <laurie801@gmail.com>
2024-02-14 15:06:30 -0800

    Create NoteView component

--- Seedling/Core/Detail/Views/NoteView.swift
+++ Seedling/Core/Detail/Views/NoteView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct NoteView: View {
+	
+//	let plant: Plant
+	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		VStack(alignment: .leading, spacing: 10) {
+			Text("February 14, 2023: 3:05pm")
+				.font(.handjet(.regular, size: 16))
+				.foregroundStyle(Color.theme.textPrimary.opacity(0.50))
+			
+			Text("They've finally sprouted!")
+				.font(.handjet(.bold, size: 24))
+				.foregroundStyle(Color.theme.textPrimary)
+			
+			Text("I didn't think they were going to survive. What a pleasant surprise. Maybe I should stick them outside for a few hours everyday to get the stems to thicken up a bit. Hope no pests get to them.")
+				.font(.handjet(.regular, size: 18))
+				.foregroundStyle(Color.theme.textPrimary)
+		}
+		.padding()
+		.background(Color.theme.backgroundAccent)
+		.clipShape(RoundedRectangle(cornerRadius: 8))
     }
 }
 
