c14a0c01277c519c12c215b22e9b7ef37c35f150
Laurie Cai <laurie801@gmail.com>
2024-02-14 14:57:07 -0800

    Add title attribute to Note entitiy

---
+++ Seedling/Core/Detail/Views/NoteView.swift
@@ -0,0 +1,18 @@
+//
+//  NoteView.swift
+//  Seedling
+//
+//  Created by Laurie Cai on 2/14/24.
+//
+
+import SwiftUI
+
+struct NoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteView()
+}
