26c423218786197bde950f7f98f8266b98d207c7
Laurie Cai <laurie801@gmail.com>
2024-02-19 10:58:17 -0800

    Add formatters for day and date, date and time to Date extension

--- Seedling/Extensions/Date.swift
+++ Seedling/Extensions/Date.swift
@@ -9,22 +9,52 @@ import Foundation
 
 extension Date {
 	
-	/// Creates a formatter that converts a Date into the desired format based on local timezone
-	private var dateFormatter: DateFormatter {
+	/// Creates a formatter that converts a Date into a baseline format based on local timezone
+	private func createFormatter() -> DateFormatter {
 		let formatter = DateFormatter()
 		formatter.timeZone = TimeZone.current
 		formatter.locale = Locale.current
+		
+		return formatter
+	}
+	
+	/// Converts a date into day of the week and date format
+	/// ```
+	/// Converts 2022-02-19 12:34:56 +0000 to Monday, February 19
+	/// ```
+	private var dayAndDateFormatter: DateFormatter {
+		let formatter = createFormatter()
 		formatter.dateFormat = "EEEE, MMMM d"
 		
 		return formatter
 	}
 	
-	/// Formats current date into day of the week, month and day of the month
+	/// Converts a date into date and time format
 	/// ```
-	/// Converts Monday, February 5, 2024 at 7:38:40 Pacific Standard Time into Monday, February 5
+	/// Converts 2022-02-19 12:34:56 +0000 to February 19, 2023 12:34pm
 	/// ```
-	func withDayAndDate() -> String {
+	private var dateAndTimeFormatter: DateFormatter {
+		let formatter = createFormatter()
+		formatter.dateFormat = "MMMM d, yyyy h:mma"
+		
+		return formatter
+	}
+	
+	/// Converts a day-and-date-formatted date into a String
+	/// ```
+	/// Converts Monday, February 19 to "Monday, February 19"
+	/// ```
+	func asDayAndDate() -> String {
 		let currentDate = Date()
-		return dateFormatter.string(from: currentDate)
+		return dayAndDateFormatter.string(from: currentDate)
+	}
+	
+	/// Converts a date-and-time-formatted date into a String
+	/// ```
+	/// Converts February 19, 2023 12:34pm to "February 19, 2023 12:34pm"
+	/// ```
+	func asDateAndTime() -> String {
+		let currentDate = Date()
+		return dateAndTimeFormatter.string(from: currentDate)
 	}
 }
