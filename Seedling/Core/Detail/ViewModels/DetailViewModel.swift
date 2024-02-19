599b06bfedfbac6e3505fe3b5f86e7b241e601ce
Laurie Cai <laurie801@gmail.com>
2024-02-19 11:02:18 -0800

    Add functionality to add, save, and fetch notes from Core Data

--- Seedling/Core/Detail/ViewModels/DetailViewModel.swift
+++ Seedling/Core/Detail/ViewModels/DetailViewModel.swift
@@ -13,8 +13,43 @@ class DetailViewModel: ObservableObject {
 	let manager = CoreDataManager.shared
 	
 	@Published var plant: Plant
+	@Published var notes: [Note] = []
 	
 	init(plant: Plant) {
 		self.plant = plant
+		print("detail view model assigned \(plant.wrappedName) to plant var")
+		fetchNotes(for: plant)
+	}
+	
+	private func fetchNotes(for plant: Plant) {
+		print("in fetchNotes(for plant: Plant) function")
+		let request = NSFetchRequest<Note>(entityName: "Note")
+		request.predicate = NSPredicate(format: "plant == %@", plant)
+		
+		do {
+			notes = try manager.context.fetch(request)
+			print("notes fetched")
+		} catch let error {
+			print("Error fetching plants from Core Data. \(error)")
+		}
+	}
+	
+	private func save() {
+		manager.save()
+		fetchNotes(for: plant)
+	}
+	
+	func addNote(plant: Plant) {
+		let newNote = Note(context: manager.context)
+		newNote.plant = plant
+		newNote.id = UUID()
+		newNote.timestamp = Date()
+		newNote.title = "here's a note about my \(plant.wrappedName)!"
+		newNote.body = "here's a description of something that happened today. this will probably contain events in the future but for now it's just free text."
+		newNote.offset = 0
+		
+		save()
 	}
 }
+
+
