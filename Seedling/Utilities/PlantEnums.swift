615a46bedd3e4e359f9e9da0ce30098a1c838c0a
Laurie Cai <laurie801@gmail.com>
2024-02-26 11:27:28 -0800

    Create PlantStage and PlantType enums

---
+++ Seedling/Utilities/PlantEnums.swift
@@ -0,0 +1,23 @@
+//
+//  PlantEnums.swift
+//  Seedling
+//
+//  Created by Laurie Cai on 2/26/24.
+//
+
+import Foundation
+
+enum PlantStage: String {
+	case seed = "Seed"
+	case seedling = "Seedling"
+	case bulb = "Bulb"
+	case transplant = "Transplant"
+}
+
+enum PlantType: String {
+	case vegetable = "Vegetable"
+	case fruit = "Fruit"
+	case herb = "Herb"
+	case flower = "Flower"
+	case shrub = "Shrub"
+}
