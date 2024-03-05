4440cf6c6e43e852824f2ea3075e3b08d58bc206
Laurie Cai <laurie801@gmail.com>
2024-03-05 10:59:39 -0800

    Add functions to fetch, save, and add new note

--- Seedling/Core/Journal/ViewModels/JournalViewModel.swift
+++ Seedling/Core/Journal/ViewModels/JournalViewModel.swift
@@ -5,4 +5,44 @@
 //  Created by Laurie Cai on 3/5/24.
 //
 
+import CoreData
 import Foundation
+
+class JournalViewModel: ObservableObject {
+	
+	let manager = CoreDataManager.shared
+	
+	@Published var allNotes: [Note] = []
+	
+	init() {
+		fetchAllNotes()
+	}
+	
+	private func fetchAllNotes() {
+		let request = NSFetchRequest<Note>(entityName: "Note")
+		let sortDescriptor = NSSortDescriptor(key: "timestamp", ascending: false)
+		request.sortDescriptors = [sortDescriptor]
+		
+		do {
+			allNotes = try manager.context.fetch(request)
+		} catch let error {
+			print("Error fetching notes from Core Data. \(error)")
+		}
+	}
+	
+	private func save() {
+		manager.save()
+	}
+	
+	func addNote(plant: Plant, title: String, body: String) {
+		let newNote = Note(context: manager.context)
+		newNote.plant = plant
+		newNote.id = UUID()
+		newNote.timestamp = Date()
+		newNote.title = title
+		newNote.body = body
+		newNote.offset = 0
+		
+		save()
+	}
+}
