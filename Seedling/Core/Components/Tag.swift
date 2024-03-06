575fbe9748627845524baa97a5dd4f6f9f426968
Laurie Cai <laurie801@gmail.com>
2024-03-06 15:37:15 -0800

    Create tag component

---
+++ Seedling/Core/Components/Tag.swift
@@ -0,0 +1,18 @@
+//
+//  Tag.swift
+//  Seedling
+//
+//  Created by Laurie Cai on 3/6/24.
+//
+
+import SwiftUI
+
+struct Tag: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tag()
+}
