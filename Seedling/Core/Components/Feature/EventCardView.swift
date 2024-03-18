13c21c0d801699ea441b6ed22315028671fc3dd4
Laurie Cai <laurie801@gmail.com>
2024-03-18 16:09:56 -0700

    Create event card component

--- Seedling/Core/Components/Feature/EventCardView.swift
+++ Seedling/Core/Components/Feature/EventCardView.swift
@@ -8,11 +8,27 @@
 import SwiftUI
 
 struct EventCardView: View {
+	
+	let previousStage: String
+	let newStage: String
+	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		HStack() {
+			Text(previousStage)
+				.fontWeight(.bold)
+			Text("to")
+			Text(newStage)
+				.fontWeight(.bold)
+			Spacer()
+		}
+		.padding()
+		.frame(maxWidth: .infinity)
+		.background(Color.theme.backgroundDark)
+		.font(.handjet(.regular, size: 18))
+		.foregroundStyle(Color.theme.textLight)
     }
 }
 
 #Preview {
-    EventCardView()
+	EventCardView(previousStage: "Seed", newStage: "Seedling")
 }
