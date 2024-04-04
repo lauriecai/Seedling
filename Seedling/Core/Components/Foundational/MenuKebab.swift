4408f65374bcf04988c4a0a0e9969400b9f12712
Laurie Cai <laurie801@gmail.com>
2024-04-04 11:18:31 -0700

    Create kebab menu component

---
+++ Seedling/Core/Components/Foundational/MenuKebab.swift
@@ -0,0 +1,18 @@
+//
+//  MenuKebab.swift
+//  Seedling
+//
+//  Created by Laurie Cai on 4/4/24.
+//
+
+import SwiftUI
+
+struct MenuKebab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuKebab()
+}
