28af6d6f2db0bd7977aac7820341256b81f090e6
Laurie Cai <laurie801@gmail.com>
2024-04-11 13:12:24 -0700

    Consolidate notes and events into posts

--- Seedling/Core/Detail/ViewModels/DetailViewModel.swift
+++ Seedling/Core/Detail/ViewModels/DetailViewModel.swift
@@ -13,10 +13,6 @@ class DetailViewModel: ObservableObject {
 	let manager = CoreDataManager.shared
 	
 	@Published var plant: Plant
-	@Published var notes: [Note] = []
-	@Published var events: [Event] = []
-	
-	/// An array of combined notes and events
 	@Published var posts: [PlantPost] = []
 	
 	init(plant: Plant) {
@@ -26,81 +22,61 @@ class DetailViewModel: ObservableObject {
 	//	MARK: - Post functions
 	
 	func fetchPosts(for plant: Plant) {
-		fetchNotes(for: plant)
-		fetchEvents(for: plant)
-		assemblePosts(for: plant)
-	}
-	
-	private func resetPosts() {
-		posts = []
-	}
-	
-	private func assemblePosts(for plant: Plant) {
+		posts.removeAll()
 		
-		resetPosts()
+		let notes = fetchNotes(for: plant) ?? []
+		let events = fetchEvents(for: plant) ?? []
 		
-		for event in events {
-			posts.append(PlantPost(entity: .event(event)))
-		}
+		let notesAndEvents = notes.map { PlantPost(entity: .note($0)) } + events.map { PlantPost(entity: .event($0)) }
 		
-		for note in notes {
-			posts.append(PlantPost(entity: .note(note)))
-		}
-		
+		posts.append(contentsOf: notesAndEvents)
 		posts.sort { $0.timestamp > $1.timestamp }
 	}
 	
 //	MARK: - Plant functions
-//	Data needs to be refetched every time a change has been saved to Core Data
 	
-	/// Updates the plant stage
 	func updatePlant(plant: Plant, newStage: PlantStage) {
 		manager.updatePlant(plant: plant, newStage: newStage.rawValue)
 	}
 	
 //	MARK: - Note functions
-//	Data needs to be refetched every time a change has been saved to Core Data
 	
-	/// Fetches the most up-to-date notes for a specified plant from Core Data
-	func fetchNotes(for plant: Plant) {
+	private func fetchNotes(for plant: Plant) -> [Note]? {
 		let request = manager.requestNotes(for: plant)
 		
 		do {
-			notes = try manager.context.fetch(request)
+			return try manager.context.fetch(request)
 		} catch let error {
 			print("Error fetching notes from Core Data. \(error)")
+			return nil
 		}
 	}
 	
-	/// Creates a new note associated with a specific plant and saves to Core Data, then refreshes the notes array
 	func addNote(for plant: Plant, title: String, body: String) {
 		manager.addNote(for: plant, title: title, body: body)
-		fetchNotes(for: plant)
+		fetchPosts(for: plant)
 	}
 	
-	/// Deletes a note from Core Data, then fetches notes for a specific plant
 	func deleteNote(note: Note) {
 		manager.deleteNote(note: note)
-		fetchNotes(for: plant)
+		fetchPosts(for: plant)
 	}
 	
 //	MARK: - Event functions
-//	Data needs to be refetched every time a change has been saved to Core Data
 	
-	/// Fetches the most up-to-date events for a specified plant from Core Data
-	func fetchEvents(for plant: Plant) {
+	private func fetchEvents(for plant: Plant) -> [Event]? {
 		let request = manager.requestEvents(for: plant)
 		
 		do {
-			events = try manager.context.fetch(request)
+			return try manager.context.fetch(request)
 		} catch let error {
 			print("Error fetching events from Core Data. \(error)")
+			return nil
 		}
 	}
 	
-	/// Deletes an event from Core Data, then fetches events for a specific plant
 	func deleteEvent(event: Event) {
 		manager.deleteEvent(event: event)
-		fetchEvents(for: plant)
+		fetchPosts(for: plant)
 	}
 }
