74a89709e954d3f98099c830589261015e131b89
Laurie Cai <laurie801@gmail.com>
2024-04-18 10:14:33 -0700

    Shorten plant stage definitions

--- Seedling/Models/PlantEnums.swift
+++ Seedling/Models/PlantEnums.swift
@@ -23,16 +23,16 @@ enum PlantStage: String, CaseIterable, RawRepresentable {
 	var definition: String {
 		switch self {
 		case .seed: "A small object from which a new plant can grow."
-		case .germination: "The process by which a seed transforms into a young plant, marked by the emergence of a root and shoot."
-		case .seedling: "A young plant that has recently sprouted from seed, characterized by the emergence of its first set of leaves and roots."
-		case .bulb: "A short, underground structure of a plant consisting of a modified shoot."
-		case .transplant: "The process of moving a growing plant from one location to another, allowing its development in a new environment."
-		case .vegetativeGrowth: "A phase in a plant's life cycle where it focuses on developing stems, leaves, and roots, contributing to overall plant size and structure."
-		case .flowering: "A stage in a plant's life cycle where it produces flowers, marking the transition to the reproductive phase with the potential for seed or fruit formation."
-		case .pollination: "The process by which pollen from the male reproductive organs of a flower (anther) is transferred to the female reproductive organs (stigma), facilitating fertilization and the production of seeds."
-		case .fruitFormation: "The process in which the fertilized ovary of a flower develops into a mature fruit, enclosing and protecting seeds that have the potential to grow into new plants."
-		case .vegetableFormation: "The development of edible plant parts such as roots, leaves, stems, or flowers, derived from the vegetative structures of a plant"
-		case .harvested: "The act of gathering and collecting mature crops or plants."
+		case .germination: "The emergence of a tiny root and shoot."
+		case .seedling: "A young plant with its first set of leaves."
+		case .bulb: "An underground bulb from which a new plant can grow."
+		case .transplant: "The process of moving a plant to a new environment."
+		case .vegetativeGrowth: "The growth of leaves, stems, and roots establishing the plant."
+		case .flowering: "A stage where the plant produces flowers."
+		case .pollination: "A period of pollen transfer leading to plant fertilization."
+		case .fruitFormation: "The development of edible fruits."
+		case .vegetableFormation: "The development of edible greens."
+		case .harvested: "The act of gathering mature plants."
 		}
 	}
 	
@@ -46,8 +46,8 @@ enum PlantStage: String, CaseIterable, RawRepresentable {
 		case .vegetativeGrowth: "has started developing leaves."
 		case .flowering: "has begun flowering."
 		case .pollination: "is undergoing pollination."
-		case .fruitFormation: "is going through fruit formation."
-		case .vegetableFormation: "is going through vegetable formation."
+		case .fruitFormation: "is developing fruit."
+		case .vegetableFormation: "is developing edible greens."
 		case .harvested: "has been harvested!"
 		}
 	}
@@ -59,4 +59,5 @@ enum PlantType: String, CaseIterable {
 	case herb = "Herb"
 	case flower = "Flower"
 	case shrub = "Shrub"
+	case succulent = "Succulent"
 }
