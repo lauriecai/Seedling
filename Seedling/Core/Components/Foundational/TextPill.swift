8b72d22e138ec05fa538a132803dd65d0d41f389
Laurie Cai <laurie801@gmail.com>
2024-05-01 14:47:18 -0700

    Create TextPill component

---
+++ Seedling/Core/Components/Foundational/TextPill.swift
@@ -0,0 +1,27 @@
+//
+//  TextPill.swift
+//  Seedling
+//
+//  Created by Laurie Cai on 5/1/24.
+//
+
+import SwiftUI
+
+struct TextPill: View {
+	
+	let label: String
+	let backgroundColor: Color
+	
+    var body: some View {
+		Text(label)
+			.font(.handjet(.bold, size: 18))
+			.padding(5)
+			.padding(.horizontal, 3)
+			.background(backgroundColor)
+			.clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+#Preview {
+	TextPill(label: "New Stage", backgroundColor: Color.theme.accentYellow)
+}
