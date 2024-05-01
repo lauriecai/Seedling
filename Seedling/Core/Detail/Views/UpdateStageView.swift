f0afddfaee5801176596fdb8e2035a3d31d2f1eb
Laurie Cai <laurie801@gmail.com>
2024-05-01 14:06:11 -0700

    Create initial view to update stage

---
+++ Seedling/Core/Detail/Views/UpdateStageView.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateStageView.swift
+//  Seedling
+//
+//  Created by Laurie Cai on 5/1/24.
+//
+
+import SwiftUI
+
+struct UpdateStageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateStageView()
+}
