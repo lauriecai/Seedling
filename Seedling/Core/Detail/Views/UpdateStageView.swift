db979762f838b5017058c571210c20d7cb959bf5
Laurie Cai <laurie801@gmail.com>
2024-05-07 12:16:46 -0700

    Add functionality to update plant stage

--- Seedling/Core/Detail/Views/UpdateStageView.swift
+++ Seedling/Core/Detail/Views/UpdateStageView.swift
@@ -7,12 +7,82 @@
 
 import SwiftUI
 
+struct UpdateStageLoadingView: View {
+	
+	let viewModel: DetailViewModel
+	
+	var body: some View {
+		UpdateStageView(viewModel: viewModel)
+	}
+}
+
 struct UpdateStageView: View {
+	
+	@ObservedObject var viewModel: DetailViewModel
+	
+	@Environment(\.dismiss) var dismiss
+	
+	init(viewModel: DetailViewModel) {
+		self.viewModel = viewModel
+	}
+	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		NavigationStack {
+			ZStack {
+				Color.theme.backgroundPrimary
+					.ignoresSafeArea()
+				
+				VStack {
+					updateStagePrompt
+
+					CardSelectionList(
+						items: PlantStage.allCases,
+						selectedPillLabel: "Selected",
+						selectedItem: $viewModel.plantStage,
+						selectedItemIndex: $viewModel.selectedStageIndex
+					)
+				}
+				.padding()
+			}
+			.navigationTitle("Update Stage")
+			.navigationBarTitleDisplayMode(.inline)
+			.navigationBarBackButtonHidden(true)
+			.toolbar {
+				ToolbarItem(placement: .topBarLeading) { cancelButton }
+				ToolbarItem(placement: .topBarTrailing) { saveChangesButton }
+			}
+			.onChange(of: viewModel.plantStage) { viewModel.plantStageUpdated = true }
+		}
     }
 }
 
-#Preview {
-    UpdateStageView()
+extension UpdateStageView {
+	
+	private var updateStagePrompt: some View {
+		Text("Select a new stage:")
+			.font(.handjet(.extraBold, size: 26))
+			.foregroundStyle(Color.theme.textPrimary)
+			.frame(maxWidth: .infinity, alignment: .leading)
+	}
+	
+	private var saveChangesButton: some View {
+		Button("Save Changes") {
+			viewModel.updatePlantStage(plant: viewModel.plant, newStage: viewModel.plantStage)
+			dismiss()
+		}
+		.font(.handjet(.extraBold, size: 20))
+		.foregroundStyle(viewModel.plantStageUpdated ? Color.theme.accentGreen : Color.theme.textSecondary.opacity(0.5))
+		.disabled(!viewModel.plantStageUpdated)
+	}
+	
+	private var cancelButton: some View {
+		Button {
+			viewModel.resetStageUpdatedFlag()
+			dismiss()
+		} label: {
+			Text("Cancel")
+				.font(.handjet(.medium, size: 20))
+				.foregroundStyle(Color.theme.textSecondary)
+		}
+	}
 }
