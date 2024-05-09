6f00769934b765a222a6c83b32bf12e13b8bc8e2
Laurie Cai <laurie801@gmail.com>
2024-05-09 11:26:11 -0700

    Configure PhotoPicker coordinator

--- Seedling/Utilities/PhotoPickerCoordinator.swift
+++ Seedling/Utilities/PhotoPickerCoordinator.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import SwiftUI
+
+class PhotoPickerCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+	
+	@Binding var image: UIImage?
+	@Binding var isShown: Bool
+	
+	init(image: Binding<UIImage?>, isShown: Binding<Bool>) {
+		_image = image
+		_isShown = isShown
+	}
+	
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		
+		if let uiImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+			image = uiImage
+			isShown = false
+		}
+	}
+	
+	func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+		isShown = false
+	}
+}
