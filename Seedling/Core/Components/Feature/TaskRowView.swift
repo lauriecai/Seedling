b8c75cf3ba1a356afff86f8413faf2e4f7ba3f6b
Laurie Cai <laurie801@gmail.com>
2024-05-22 17:24:40 -0700

    Add checkmark icon

---
+++ Seedling/Core/Components/Feature/TaskRowView.swift
@@ -0,0 +1,24 @@
+//
+//  TaskRowView.swift
+//  Seedling
+//
+//  Created by Laurie Cai on 5/22/24.
+//
+
+import SwiftUI
+
+struct TaskRowView: View {
+	
+	let taskTitle: String
+	let isComplete: Bool
+	
+    var body: some View {
+		HStack {
+			
+		}
+    }
+}
+
+#Preview {
+    TaskRowView()
+}
