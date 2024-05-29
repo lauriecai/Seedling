95088a373c6e3d9c58d5f299a811cfb6cea45720
Laurie Cai <laurie801@gmail.com>
2024-05-28 21:57:44 -0700

    Create CategorySelectionList componenent for task category assignment

---
+++ Seedling/Core/Components/Feature/CategorySelectionList.swift
@@ -0,0 +1,43 @@
+//
+//  CategorySelectionList.swift
+//  Seedling
+//
+//  Created by Laurie Cai on 5/28/24.
+//
+
+import SwiftUI
+
+struct CategorySelectionList: View {
+	
+	let viewModel: TasksViewModel
+	
+	let categories: [TaskCategory]
+	
+	let accentTheme: Bool
+	
+	let selectedPillLabel: String
+	@Binding var selectedCategory: TaskCategory?
+	@Binding var selectedCategoryIndex: Int
+	
+    var body: some View {
+		ScrollView(showsIndicators: false) {
+			VStack(alignment: .leading, spacing: 10) {
+				ForEach(Array(categories.enumerated()), id: \.1) { index, category in
+					CardSelectable(
+						title: category.wrappedName,
+						description: nil,
+						accentTheme: accentTheme,
+						isSelected: index == selectedCategoryIndex,
+						selectedPillLabel: selectedPillLabel
+					)
+					.onTapGesture {
+						selectedCategory = category
+						selectedCategoryIndex = index
+						viewModel.taskCategoryInput = category.wrappedName
+						viewModel.selectedAddTaskViewIndex = 0
+					}
+				}
+			}
+		}
+    }
+}
---
+++ Seedling/Core/Components/Feature/StageSelectionList.swift
@@ -0,0 +1,18 @@
+//
+//  StageSelectionList.swift
+//  Seedling
+//
+//  Created by Laurie Cai on 5/28/24.
+//
+
+import SwiftUI
+
+struct StageSelectionList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StageSelectionList()
+}
