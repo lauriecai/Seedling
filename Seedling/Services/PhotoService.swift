3cf7a332e1269d728f3ce5975e1b68f1e55ac37c
Laurie Cai <laurie801@gmail.com>
2024-06-27 13:22:17 -0700

    Create PhotoService

---
+++ Seedling/Services/PhotoService.swift
@@ -0,0 +1,44 @@
+//
+//  PhotoService.swift
+//  Seedling
+//
+//  Created by Laurie Cai on 6/27/24.
+//
+
+import CoreData
+import Foundation
+
+// MARK: - Description
+// Responsible for photo CRUD operations
+
+class PhotoService {
+	
+	private let coreDataManager = CoreDataManager.shared
+	
+//	MARK: - Methods
+	
+	func fetchPhotos() -> [Photo] {
+		let request = createPhotoRequest()
+		
+		do {
+			return try coreDataManager.context.fetch(request)
+		} catch {
+			print("Error fetching images. \(error.localizedDescription)")
+			return []
+		}
+	}
+	
+	func createPhoto(caption: String) -> Photo {
+		let newPhoto = Photo(context: coreDataManager.context)
+		newPhoto.imageUrlString = UUID().uuidString
+		newPhoto.timestamp = Date()
+		
+		return newPhoto
+	}
+	
+	private func createPhotoRequest() -> NSFetchRequest<Photo> {
+		let request = NSFetchRequest<Photo>(entityName: "Photo")
+		
+		return request
+	}
+}
