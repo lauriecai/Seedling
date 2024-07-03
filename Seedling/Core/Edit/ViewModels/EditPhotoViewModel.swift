686658069bd9aaca1c6fe3e33598306d5ac3eb41
Laurie Cai <laurie801@gmail.com>
2024-07-02 17:06:15 -0700

    Create EditPhotoView and EditPhotoViewModel

---
+++ Seedling/Core/Edit/ViewModels/EditPhotoViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  EditPhotoViewModel.swift
+//  Seedling
+//
+//  Created by Laurie Cai on 7/2/24.
+//
+
+import Foundation
---
+++ Seedling/Core/Edit/Views/EditPhotoView.swift
@@ -0,0 +1,18 @@
+//
+//  EditPhotoView.swift
+//  Seedling
+//
+//  Created by Laurie Cai on 7/2/24.
+//
+
+import SwiftUI
+
+struct EditPhotoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditPhotoView()
+}
