16fac2a186729049fde2ff55461f89edb8bfc38a
Laurie Cai <laurie801@gmail.com>
2024-07-09 10:05:08 -0700

    Create initial EditPhotoView

--- Seedling/Core/Edit/Views/EditPhotoView.swift
+++ Seedling/Core/Edit/Views/EditPhotoView.swift
@@ -5,14 +5,75 @@
 //  Created by Laurie Cai on 7/2/24.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct EditPhotoView: View {
+	
+	@ObservedObject var viewModel: EditPhotoViewModel
+	@ObservedObject var imagePickerService: ImagePickerService
+	
+	@Environment(\.dismiss) var dismiss
+	
+	let coreDataManager = CoreDataManager.shared
+	let fileManager = FileManager()
+	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		NavigationStack {
+			VStack {
+				Image(uiImage: viewModel.image)
+					.resizable()
+					.scaledToFit()
+				
+				TextField("Description", text: $viewModel.caption)
+				
+				HStack {
+					if viewModel.editingExistingImage {
+						PhotosPicker("Edit Image",
+							selection: $imagePickerService.selectedPhotosPickerItem,
+							matching: .images,
+							photoLibrary: .shared())
+					}
+					
+					Button {
+						if viewModel.editingExistingImage {
+							viewModel.saveChanges()
+						} else {
+							viewModel.createPhoto()
+						}
+						dismiss()
+					} label: {
+						Text("Save")
+					}
+				}
+				
+				Spacer()
+			}
+			.navigationTitle(viewModel.editingExistingImage ? "Edit Photo" : "New Photo")
+			.navigationBarTitleDisplayMode(.inline)
+			.toolbar {
+				ToolbarItem(placement: .topBarLeading) {
+					Button("Cancel") { dismiss() }
+				}
+				
+				if viewModel.editingExistingImage {
+					ToolbarItem(placement: .topBarTrailing) {
+						Button {
+							viewModel.deletePhoto()
+							dismiss()
+						} label: {
+							Image(systemName: "trash")
+						}
+					}
+				}
+			}
+			.onChange(of: imagePickerService.selectedImage) { _, newImage in
+				if let newImage { viewModel.image = newImage }
+			}
+		}
     }
 }
 
 #Preview {
-    EditPhotoView()
+	EditPhotoView(viewModel: EditPhotoViewModel(newImage: UIImage(systemName: "photo")!), imagePickerService: ImagePickerService())
 }
---
+++ Seedling/Models/EditPhotoMode.swift
@@ -0,0 +1,8 @@
+//
+//  EditPhotoMode.swift
+//  Seedling
+//
+//  Created by Laurie Cai on 7/9/24.
+//
+
+import Foundation
