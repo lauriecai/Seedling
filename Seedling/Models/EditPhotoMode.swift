b3fe71f0fedd035e2527dcd1e8dbbf8ba9fda1c0
Laurie Cai <laurie801@gmail.com>
2024-07-09 10:12:45 -0700

    Create EditPhotoMode enum for distinction between create and edit

--- Seedling/Models/EditPhotoMode.swift
+++ Seedling/Models/EditPhotoMode.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import SwiftUI
+
+enum EditPhotoMode: Identifiable, View {
+	case create(UIImage)
+	case edit(Photo)
+	
+	var id: String {
+		switch self {
+		case .create: return "create"
+		case .edit: return "edit"
+		}
+	}
+	
+	var body: some View {
+		switch self {
+		case .create(let selectedImage):
+			EditPhotoView(viewModel: EditPhotoViewModel(newImage: selectedImage))
+		case .edit(let savedPhoto):
+			EditPhotoView(viewModel: EditPhotoViewModel(existingPhoto: savedPhoto))
+		}
+	}
+}
