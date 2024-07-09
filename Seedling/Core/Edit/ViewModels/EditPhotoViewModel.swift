a1a850e758109c5426fefcbef5949adb593d63bd
Laurie Cai <laurie801@gmail.com>
2024-07-09 09:25:54 -0700

    Add functionality fo EditPhotoViewModel

--- Seedling/Core/Edit/ViewModels/EditPhotoViewModel.swift
+++ Seedling/Core/Edit/ViewModels/EditPhotoViewModel.swift
@@ -8,18 +8,66 @@
 import Foundation
 import SwiftUI
 
-enum EditPhotoMode: Identifiable, View {
-	case create(UIImage)
-	case edit(Photo)
+class EditPhotoViewModel: ObservableObject {
 	
-	var id: String {
-		switch self {
-		case .create: return "create"
-		case .edit: return "edit"
-		}
+	@Published var image: UIImage
+	@Published var caption: String = ""
+	
+	var imageUrlString: String?
+	var editingExistingImage: Bool { imageUrlString != nil }
+	
+	let coreDataManager = CoreDataManager.shared
+	let fileManager = FileManager()
+	
+	let photoService = PhotoService()
+	
+//	MARK: - Init
+	
+	init(newImage: UIImage) {
+		self.image = newImage
 	}
 	
-	var body: some View {
+	init(existingImage: Photo) {
+		self.image = existingImage.uiImage
+		self.caption = existingImage.wrappedCaption
+		self.imageUrlString = existingImage.wrappedImageUrlString
+	}
+	
+//	MARK: - Methods
+	
+	func createPhoto() {
+		let newPhoto = photoService.createPhoto(caption: caption)
 		
+		coreDataManager.save()
+		fileManager.saveImage(id: newPhoto.wrappedImageUrlString, image: image)
+	}
+	
+	func saveChanges() {
+		guard let imageUrlString,
+			  let savedPhoto = findExistingPhoto() else { return }
+		
+		savedPhoto.caption = caption
+		
+		coreDataManager.save()
+		fileManager.saveImage(id: imageUrlString, image: image)
+	}
+	
+	func deletePhoto() {
+		guard let imageUrlString,
+			  let savedPhoto = findExistingPhoto() else { return }
+		
+		fileManager.deleteImage(id: imageUrlString)
+		coreDataManager.deletePhoto(photo: savedPhoto)
+	}
+	
+	private func fetchPhotos() -> [Photo] {
+		photoService.fetchPhotos()
+	}
+	
+	private func findExistingPhoto() -> Photo? {
+		let allPhotos = fetchPhotos()
+		
+		if let existingPhoto = allPhotos.first(where: { $0.imageUrlString == imageUrlString }) 
+		{ return existingPhoto } else { return nil }
 	}
 }
