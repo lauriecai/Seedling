175738bd038faf336dd9ea85238b3137defa98ac
Laurie Cai <laurie801@gmail.com>
2024-07-10 10:03:52 -0700

    Add showingPhotosPicker flag

--- Seedling/Core/Detail/ViewModels/DetailViewModel.swift
+++ Seedling/Core/Detail/ViewModels/DetailViewModel.swift
@@ -68,6 +68,9 @@ class DetailViewModel: ObservableObject {
 	@Published var editingCareRequirements: Bool = false
 	@Published var editingAdditionalCareNotes: Bool = false
 	
+	// Photo Picker Segues
+	@Published var showingPhotosPicker: Bool = false
+	
 	init(plant: Plant) {
 		self.plant = plant
 		
