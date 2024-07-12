da588c81c05b70a22c4b133a983b2a218a278835
Laurie Cai <laurie801@gmail.com>
2024-07-11 17:49:46 -0700

    Build out EditPhotoView UI

--- Seedling/Core/Edit/Views/EditPhotoView.swift
+++ Seedling/Core/Edit/Views/EditPhotoView.swift
@@ -20,61 +20,65 @@ struct EditPhotoView: View {
 	let fileManager = FileManager()
 	
     var body: some View {
-		NavigationStack {
-			VStack {
-				Image(uiImage: viewModel.image)
-					.resizable()
-					.scaledToFit()
-				
-				TextField("Description", text: $viewModel.caption)
-				
-				HStack {
-					if viewModel.editingExistingImage {
-						PhotosPicker("Edit Image",
-							selection: $imagePickerService.selectedPhotosPickerItem,
-							matching: .images,
-							photoLibrary: .shared())
-					}
+		ZStack {
+			Color.theme.backgroundPrimary
+				.ignoresSafeArea()
+			
+			ScrollView(showsIndicators: false) {
+				VStack(spacing: 15) {
+					Image(uiImage: viewModel.image)
+						.resizable()
+						.scaledToFit()
+						.frame(height: 300)
+						.clipShape(RoundedRectangle(cornerRadius: 8))
 					
-					Button {
-						if viewModel.editingExistingImage {
-							viewModel.saveChanges()
-						} else {
-							viewModel.createPhoto()
-						}
-						dismiss()
-					} label: {
-						Text("Save")
-					}
+					noteBodyInput
+					
+					Spacer()
 				}
-				
-				Spacer()
+				.padding(.horizontal)
 			}
-			.navigationTitle(viewModel.editingExistingImage ? "Edit Photo" : "New Photo")
-			.navigationBarTitleDisplayMode(.inline)
-			.toolbar {
-				ToolbarItem(placement: .topBarLeading) {
-					Button("Cancel") { dismiss() }
-				}
-				
-				if viewModel.editingExistingImage {
-					ToolbarItem(placement: .topBarTrailing) {
-						Button {
-							viewModel.deletePhoto()
-							dismiss()
-						} label: {
-							Image(systemName: "trash")
-						}
-					}
-				}
-			}
-			.onChange(of: imagePickerService.selectedImage) { _, newImage in
-				if let newImage { viewModel.image = newImage }
-			}
+		}
+		.navigationTitle(viewModel.editingExistingImage ? "Edit Photo" : "New Photo")
+		.navigationBarTitleDisplayMode(.inline)
+		.navigationBarBackButtonHidden(true)
+		.toolbar {
+			ToolbarItem(placement: .topBarLeading) { cancelButton }
+			ToolbarItem(placement: .topBarTrailing) { addPhotoButton }
+		}
+		.keyboardType(.default)
+		.onChange(of: imagePickerService.selectedImage) { _, newImage in
+			if let newImage { viewModel.image = newImage }
 		}
     }
 }
 
 #Preview {
 	EditPhotoView(viewModel: EditPhotoViewModel(newImage: UIImage(systemName: "photo")!))
+}
+
+extension EditPhotoView {
+	
+	private var noteBodyInput: some View {
+		TextEditorInput(inputHeader: "Description", headerDescription: "Optional", inputPlaceholder: "Start writing...", accentTheme: true, text: $viewModel.caption)
+	}
+	
+	private var addPhotoButton: some View {
+		Button("Add Photo") {
+			UIImpactFeedbackGenerator(style: .light).impactOccurred()
+			print("add functionality - add, reset") // tbd
+		}
+		.font(.handjet(.extraBold, size: 20))
+		.foregroundStyle(Color.theme.accentGreen)
+	}
+	
+	private var cancelButton: some View {
+		Button {
+			dismiss()
+		} label: {
+			Text("Cancel")
+				.font(.handjet(.medium, size: 20))
+		}
+		.foregroundStyle(Color.theme.textSecondary)
+	}
 }
