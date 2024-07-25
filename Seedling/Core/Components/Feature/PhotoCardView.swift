90a35f25489e935c554efa38ea9c6f7b177dd1ca
Laurie Cai <laurie801@gmail.com>
2024-07-25 10:19:07 -0700

    Create outline of PhotoCardView

--- Seedling/Core/Components/Feature/PhotoCardView.swift
+++ Seedling/Core/Components/Feature/PhotoCardView.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct PhotoCardView: View {
+	
+	let photo: Photo
+	
+	@Binding var showActionSheet: Bool
+	@Binding var showActionsForPhoto: Photo?
+	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		VStack(alignment: .leading, spacing: 10) {
+			if !photo.wrappedCaption.isEmpty { photoCaption }
+		}
     }
 }
 
-#Preview {
-    PhotoCardView()
+extension PhotoCardView {
+	
+	private var photoCaption: some View {
+		Text(photo.wrappedCaption)
+			.font(.handjet(.medium, size: 20))
+			.foregroundStyle(Color.theme.textPrimary)
+	}
+	
+	private var timestampAndActionsFooter: some View {
+		HStack {
+			timestamp
+			Spacer()
+			photoActions
+		}
+	}
+	
+	private var timestamp: some View {
+		HStack {
+			Text(photo.wrappedTimestamp.asDateAndTime())
+				.font(.handjet(.regular, size: 18))
+				.foregroundStyle(Color.theme.textSecondary)
+			
+			Spacer()
+		}
+	}
+	
+	private var photoActions: some View {
+		Button {
+			showActionSheet = true
+			showActionsForPhoto = photo
+		} label: {
+			MenuKebab()
+				.padding(.top, 10)
+				.frame(maxHeight: .infinity, alignment: .top)
+				.opacity(0.7)
+		}
+	}
 }
